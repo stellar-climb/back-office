e7ac7d7933790e921444910612fe04ce0c9b99f2
김정호 <jeangho293@naver.com>
2025-08-12 02:02:25 +0900

    .

--- src/libs/http-client/index.ts
+++ src/libs/http-client/index.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 // ALB 주소가 아닌 CloudFront 주소를 baseURL로 사용
 const apiClient = axios.create({
   baseURL: "https://back-office.stellar-climb.com/admins", // 또는 CloudFront 도메인
+  headers: {
+    "x-stellar-climb": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+  },
 });
 
 export const getUsers = () => {
