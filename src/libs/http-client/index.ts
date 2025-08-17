2930d9e983f7a1202401c3e60239155040f1e2c8
김정호 <jeangho293@naver.com>
2025-08-17 22:25:45 +0900

    fix: env

--- src/libs/http-client/index.ts
+++ src/libs/http-client/index.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // ALB 주소가 아닌 CloudFront 주소를 baseURL로 사용
 const apiClient = axios.create({
-  baseURL: "https://back-office.stellar-climb.com/admins", // 또는 CloudFront 도메인
+  baseURL: import.meta.env.VITE_CORE_API, // 또는 CloudFront 도메인
   headers: {
     "x-stellar-climb": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
   },
 });
 
 export const getUsers = () => {
-  return apiClient.get("/health"); // 경로만 붙여서 호출
+  return apiClient.get("/users"); // 경로만 붙여서 호출
 };
